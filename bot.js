c98bb77f0d6dd99870a02f8c89ec4f3e3befe0c6
Panda Tseng <pandatseng@KronosAdmins-MacBook-Air.local>
2021-09-29 19:38:19 +0800

    update

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ bot.onText(/\/start@woobot/, (msg) => {
         ],
         [
           {
-            text: '留言活動，有機會獲得 50USD 價值 $WOO',
+            text: '留言活動，有機會獲得 50USD 價值 WOO',
           }
         ]  
       ]
@@ -81,7 +81,7 @@ bot.onText(/加入社群/, (msg, match) => {
   
     /* WOO 官方網站  reply */
     bot.onText(/留言活動，有機會獲得 50USD 價值 $WOO/, (msg, match) => {
-      bot.sendMessage(msg.chat.id, "留言活動，有機會獲得 50USD 價值 $WOO！", {
+      bot.sendMessage(msg.chat.id, "留言活動，有機會獲得 50USD 價值 WOO！", {
           "reply_markup": {
               "inline_keyboard": [
                   [
