f9d4e72a0b7350cb4eef4eff7eeab4cab613014e
Panda Tseng <pandatseng@KronosAdmins-MacBook-Air.local>
2022-01-03 11:21:50 +0800

    update

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const bot = new TelegramBot(token, { polling: true });
 var socialmedia = '手刀加入 WOO Network 台灣社群媒體，不錯過任何一則精彩消息與活動！\n\n ⚡️ Facebook：官方消息、不定時好康活動 \n ⚡️ Twitter：官方消息、資訊討論互動 \n ⚡️ Medium：產業趨勢消息、科普懶人包 \n ⚡️ LINE：資訊討論互動'
 var faq = '不管你是 KYC 或是地址證明 ..等，大小疑難雜症歡迎投信至 \n\n support@woo.network'
 var woox = '低至零手續費交易所 - WOO X'
-var woonetwork = 'https://medium.com/@WOONetwork.tw/%E9%99%A4%E4%BA%86%E8%B3%AA%E6%8A%BC-%E6%8C%81%E6%9C%89-woo-%E9%82%84%E6%9C%89%E9%80%99%E9%BA%BC%E5%A4%9A%E7%9A%84%E7%8D%B2%E7%9B%8A%E5%A0%B4%E6%99%AF-c4c5aa04ef33'
+var woonetwork = 'https://bit.ly/3HuqByp'
 var kyc = '跟我們一起來 KYC 吧！ \n\n https://bit.ly/3uBrGz2'
 
 bot.onText(/\/start@woobot/, (msg) => {
@@ -51,31 +51,31 @@ bot.onText(/加入社群/, (msg, match) => {
               [
                   {
                       text: "Facebook",
-                      url: 'https://www.facebook.com/WOONetwork.tw'
+                      url: 'https://bit.ly/3mPsCxl'
                   },
                   {
                     text: "Twitter",
-                    url: 'https://twitter.com/WOOnetwork_CN'
+                    url: 'https://bit.ly/3JylyPb'
                 },
               ],
               [
                 {
                     text: "Medium",
-                    url: 'https://medium.com/@WOONetwork.tw'
+                    url: 'https://bit.ly/34fWdcO'
                 },
                 {
                   text: "LINE",
-                  url: 'https://line.me/ti/g2/5fcFq_yjTQkMcj9YLqdGZQ?utm_source=invitation&utm_medium=link_copy&utm_campaign=default'
+                  url: 'https://bit.ly/3eQdtqR'
                 },
               ],
               [
                 {
                   text: "Telegram 公告",
-                  url: "https://t.me/WOONetworkTWann"
+                  url: "https://bit.ly/3eJOSnH"
                 },
                 {
                   text: "點此獲取更多連結",
-                  url: 'https://linktr.ee/woonetworktw'
+                  url: 'https://bit.ly/3ESTtyp'
                 }
               ],
           ],
@@ -111,7 +111,7 @@ bot.onText(/加入社群/, (msg, match) => {
                 [
                     {
                         text: "官方網站",
-                        url: 'https://woo.org/'
+                        url: 'https://bit.ly/3sWWCv7'
                     },
                 ],
             ],
@@ -162,7 +162,7 @@ bot.onText(/加入社群/, (msg, match) => {
                 [
                   {
                       text: 'WOOFi Swap',
-                      url: 'https://fi.woo.org/',
+                      url: 'https://bit.ly/3ESSX3r',
                   }
                 ]
             ],
